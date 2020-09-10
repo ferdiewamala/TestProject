a8f47c5216d1f9bd3790adf928c54f0123868c7d
ferdiewamala <ferdiewamala@gmail.com>
2020-09-10 16:36:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GaadsLeaderBoardPracticeProject
+ LeaderBoard Practice project shows the leaders in two learning categories, data is fetched from an API in Json and diplayed in a tabbed activity
---
+++ app/src/main/java/com/example/testproject/ApiUtility.java
@@ -0,0 +1,59 @@
+package com.example.testproject;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class ApiUtility {
+    private static final String QUERY_PARAMETER_KEY = "api";
+//    private static final String KEY = "key";
+//    private static final String API_KEY = "AIzaSyA3n0cn9F3JrbXRgbiWKRwbFYQRa-DiEAc";
+
+    private ApiUtility () {}
+
+    public static final String BASE_API_URL = "https://gadsapi.herokuapp.com";
+
+    public static URL buildUrl (String title) {
+        URL url = null;
+        Uri uri= Uri.parse(BASE_API_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAMETER_KEY,title)
+                .build();
+        try {
+            url = new URL(url.toString());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            return url;
+        }
+
+    }
+
+    public static String getJson(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream stream = connection.getInputStream();
+            Scanner scanner= new Scanner(stream);
+            scanner.useDelimiter("\\A");
+            boolean hasData = scanner.hasNext();
+            if(hasData) {
+                return scanner.next();
+            }
+            else {
+                return null;
+            }
+        } catch (Exception e) {
+            Log.d("Error", e.getMessage());
+            return null;
+        } finally {
+            connection.disconnect();
+        }
+    }
+}
